02c8430d0dc511ee68d83b5aa0f9cd80c275c1f4
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-08-23 15:51:03 -0300

    Revert "Merge pull request #3 from netrometro/dev-higor"
    This reverts commit 9d5ed05421d9fee5a390498f18a51cb9efbdc7bd, reversing
    changes made to ccfcbfc356e992485f58479fd67336dea9530c89.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,13 +2,12 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./src/pages/Home";
+import EcoRank from "./src/pages/EcoRank";
 import Gifts from "./src/pages/Gifts";
 import AuthScreen from "./src/pages/AuthScreen";
 import Navbar from "./src/components/Navbar";
 import { AuthScreenProps } from "./src/types/PagesTypeList";
 import UserProfile from "./src/pages/UserProfile";
-import EcoInfo from "./src/pages/EcoInfo";
-import { EcoDicas } from "./src/pages/EcoDicas";
 
 const Stack = createNativeStackNavigator();
 
@@ -19,8 +18,7 @@ function App() {
         <Stack.Screen name="AuthScreen" component={AuthScreen} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Gifts" component={Gifts} />
-        <Stack.Screen name="EcoInfo" component={EcoInfo} />
-        <Stack.Screen name="EcoDicas" component={EcoDicas} />
+        <Stack.Screen name="EcoRank" component={EcoRank} />
         <Stack.Screen name="UserProfile" component={UserProfile} />
       </Stack.Navigator>
     </NavigationContainer>
--- frontend/src/pages/EcoDicas/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export function EcoDicas(){
-    return(
-        <View>
-            <Text>Ecodicas</Text>
-        </View>
-    )
-}
