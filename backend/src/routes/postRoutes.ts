534dac9af72c702ae18aefe9e66ad82e4892112d
Luís Eduardo <luiseduardo0501@protonmail.com>
2023-08-23 14:51:05 -0300

    rotas do show posts

--- backend/src/routes/postRoutes.ts
+++ backend/src/routes/postRoutes.ts
@@ -5,5 +5,7 @@ import postController from '../controller/postController';
 export default function (fastify: FastifyInstance){
     fastify.post("/newQuote", postController.postQuote),
     fastify.put("/updatePost/:userId", postController.updatePost),
-    fastify.delete("/delPost/:userId", postController.deletePost)
+    fastify.delete("/delPost/:userId", postController.deletePost),
+    fastify.get("/Homepage/posts", postController.showAllPosts),
+    fastify.get("/Homepage/Post/:userId", postController.showPost)
 }
