fb5e33d8b374d61d2050ad8e108567bbf4cd61e5
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2023-08-23 15:13:06 -0300

    Update userRoute.ts

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -5,5 +5,5 @@ export default function (fastify: FastifyInstance) {
   fastify.post("/login", UserController.loginUser);
   fastify.post("/register", UserController.registerUser);
   fastify.get("/getAllUsers", UserController.findAllUsers);
-  fastify.delete("/deleteUser", UserController.deleteUser);
+  fastify.post("/deleteUser", UserController.deleteUser);
 }
