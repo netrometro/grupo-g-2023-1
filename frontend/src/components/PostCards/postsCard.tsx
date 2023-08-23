1a3a4ae7fb18a94c74f61293470b6afccfbaf54d
Luís Eduardo <luiseduardo0501@protonmail.com>
2023-08-23 15:41:23 -0300

    post card class created

---
+++ frontend/src/components/PostCards/postsCard.tsx
@@ -0,0 +1,13 @@
+// export default postsCard(){
+
+//     return(
+//         <div>
+//             <img src={image} alt = {name}/>
+//             <h3>{name}</h3>
+//             <p>{price}</p>
+//             <button onClick={leaveLike}>Like</button>
+//             <button onClick={comment}>Comentar</button>
+//         </div>
+
+//     )
+// }
