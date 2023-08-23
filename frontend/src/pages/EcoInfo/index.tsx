b870a32886b6d91ea40d5469325049779d1e4039
higucavalcanti <cavalcanti.higor1@gmail.com>
2023-08-23 11:38:00 -0300

    Adicionando titulo na tela EcoInfo

--- frontend/src/pages/EcoInfo/index.tsx
+++ frontend/src/pages/EcoInfo/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 import { AuthScreenProps } from "../../types/PagesTypeList";
 
+
 const EcoInfo = ({ navigation }: AuthScreenProps) => {
   return (
     <View style={styles.container}>
@@ -18,5 +19,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#1D3623",
+  },
+  title: {
+    color: "#C8E6C9",
+    fontSize: 35,
+    marginTop: 40,
+    marginLeft: 25,
+    fontWeight: "600",
   },
 });
