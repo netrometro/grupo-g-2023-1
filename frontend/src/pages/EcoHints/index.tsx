76424005bcfc04b1ae8f49143ef8cc57690f6c60
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-08-29 16:46:46 -0300

    Adicionando Titulo na aba EcoHints

--- frontend/src/pages/EcoHints/index.tsx
+++ frontend/src/pages/EcoHints/index.tsx
@@ -6,7 +6,7 @@ import { AuthScreenProps } from "../../types/PagesTypeList";
 const EcoHints = ({ navigation }: AuthScreenProps) => {
   return (
     <View style={styles.container}>
-      <Text>EcoHints</Text>
+      <Text style={styles.title}>EcoHints</Text>
       <Navbar navigation={navigation} />
     </View>
   );
@@ -18,5 +18,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#1D3623",
+  },
+  title: {
+    color: "#C8E6C9",
+    fontSize: 35,
+    marginTop: 40,
+    marginLeft: 25,
+    fontWeight: "600",
   },
 });
