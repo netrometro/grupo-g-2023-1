aacf784004a2bdf3c4c82b75266d250974f31c63
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-09-06 16:45:22 -0300

    admin infop

--- backend/src/controller/infopController.ts
+++ backend/src/controller/infopController.ts
@@ -1,153 +1,211 @@
 import {FastifyReply, FastifyRequest} from "fastify";
 import { infopSchema } from "../Schema/infopSchema";
 import { prisma } from "../prisma/prismaClient";
+import { User } from "../interfaces/userInterface";
 
-export async function createInfop (
-    request: FastifyRequest, 
-    reply: FastifyReply){
+export async function createInfop(
+    request: FastifyRequest,
+    reply: FastifyReply
+) {
+    const { email } = request.params as User;
 
-    try{
-        const {
+    const user = await prisma.usuario.findUnique({
+        where: { email: email },
+    });
+
+    if (!user) {
+        return reply
+            .status(404)
+            .send({ error: "Usuário não encontrado." });
+    }
+
+    if (!user.isAdmin) {
+        return reply
+            .status(403)
+            .send({ error: "Apenas administradores podem criar InfoPosts." });
+    }
+
+    try {
+        const { 
             title, 
             text, 
             categorypostId
-        } = request.body as infopSchema;
+         } = request.body as infopSchema;
 
-            const infop = await prisma.infoPost.create({
-                data: {
-                    title: title,
-                    text: text,
-                    categorypostId: Number(categorypostId),
-                }
-            });
+        const infop = await prisma.infoPost.create({
+            data: {
+                title: title,
+                text: text,
+                categorypostId: Number(categorypostId),
+            },
+        });
 
         return reply
-        .status(201)
-        .send(infop);
-    } catch(error) {
+            .status(201)
+            .send(infop);
+    } catch (error) {
         return reply
-        .status(500)
-        .send({ error: "Erro ao criar InfoPost"})
+            .status(500)
+            .send({ error: "Erro ao criar InfoPost" });
     }
-};
+}
 
 export async function getInfop(
-    request: FastifyRequest, 
-    reply: FastifyReply) {
-    
-    try{
-        const {infopostId} = request.params as infopSchema;
+    request: FastifyRequest,
+    reply: FastifyReply
+) {
+    try {
+        const { infopostId } = request.params as infopSchema;
 
         const infop = await prisma.infoPost.findUnique({
-        where: { 
-            infopostId: Number(infopostId) },
+            where: {
+                infopostId: Number(infopostId),
+            },
         });
-        
+
+        if (!infop) {
+            return reply
+                .status(404)
+                .send({ error: "InfoPost não encontrado" });
+        }
+
         return reply
-        .status(200)
-        .send(infop);
-    } catch(error){
+            .status(200)
+            .send(infop);
+    } catch (error) {
         return reply
-        .status(500)
-        .send({ error: "InfoPost não encontrado"})
+            .status(500)
+            .send({ error: "Erro interno do servidor" });
     }
-    
-};
+}
 
 export async function getAllInfops(
-    request: FastifyRequest, 
-    reply: FastifyReply) {
-    
-    try{
-        const infop = await prisma.infoPost.findMany();
-        
-        return reply
-        .status(200)
-        .send(infop);
-    } catch(error){
-        return reply
-        .status(500)
-        .send({ error: "Erro em listar todos os InfoPost"});
-    }
-    
-};
+    request: FastifyRequest,
+    reply: FastifyReply
+) {
+    try {
+        const infops = await prisma.infoPost.findMany();
 
-export async function updateInfop (
-    request: FastifyRequest, 
-    reply: FastifyReply){
+        return reply
+            .status(200)
+            .send(infops);
+    } catch (error) {
+        return reply
+            .status(500)
+            .send({ error: "Erro em listar todos os InfoPosts" });
+    }
+}
+
+export async function updateInfop(
+    request: FastifyRequest,
+    reply: FastifyReply
+) {
+    const { email } = request.params as User;
+
+    const user = await prisma.usuario.findUnique({
+        where: { email: email },
+    });
+
+    if (!user) {
+        return reply
+            .status(404)
+            .send({ error: "Usuário não encontrado." });
+    }
+
+    if (!user.isAdmin) {
+        return reply
+            .status(403)
+            .send({ error: "Apenas administradores podem atualizar InfoPosts." });
+    }
 
     try {
-        const {
-            infopostId,
-            title,
-            text,
-            categorypostId
-        } = request.body as infopSchema;
+        const { infopostId, title, text, categorypostId } = request.body as infopSchema;
 
         let infop = await prisma.infoPost.findUnique({
-            where: {infopostId: Number(infopostId)}
-        })
+            where: { infopostId: Number(infopostId) },
+        });
 
-        if(!infop){
+        if (!infop) {
             return reply
-            .status(404)
-            .send({ message: "InfoPost não encontrado."});
+                .status(404)
+                .send({ error: "InfoPost não encontrado." });
         }
 
         infop = await prisma.infoPost.update({
-            where:{
+            where: {
                 infopostId: Number(infopostId),
                 title: title,
-                text: text, 
-                categorypostId: Number(categorypostId)}, 
-                data: {title, text},
-        })
+                text: text,
+                categorypostId: Number(categorypostId),
+            },
+            data: { title, text },
+        });
 
         return reply
-        .status(200)
-        .send(infop)
+            .status(200)
+            .send(infop);
     } catch (error) {
         return reply
-        .status(500)
-        .send({ error: "Não foi possível atualizar InfoPost."})
-    }};
+            .status(500)
+            .send({ error: "Não foi possível atualizar InfoPost." });
+    }
+}
 
 export async function deleteInfop(
-    request: FastifyRequest, 
-    reply: FastifyReply){
-        
+    request: FastifyRequest,
+    reply: FastifyReply
+) {
+    const { email } = request.params as User;
+
+    const user = await prisma.usuario.findUnique({
+        where: { email: email },
+    });
+
+    if (!user) {
+        return reply
+            .status(404)
+            .send({ error: "Usuário não encontrado." });
+    }
+
+    if (!user.isAdmin) {
+        return reply
+            .status(403)
+            .send({ error: "Apenas administradores podem excluir InfoPosts." });
+    }
+
     try {
-        const {infopostId} = request.params as infopSchema;
-        const {title, text, categorypostId} = request.body as infopSchema;
+        const { infopostId, title, text, categorypostId } = request.body as infopSchema;
 
         let infop = await prisma.infoPost.findUnique({
             where: {
                 infopostId: Number(infopostId),
                 title: title,
                 text: text,
-                categorypostId: Number(categorypostId)}
-        })
+                categorypostId: Number(categorypostId),
+            },
+        });
 
-        if(!infop){
+        if (!infop) {
             return reply
-            .status(404)
-            .send({error: "InfoPost não encontrado."});
+                .status(404)
+                .send({ error: "InfoPost não encontrado." });
         }
 
         infop = await prisma.infoPost.delete({
-            where:{
+            where: {
                 infopostId: Number(infopostId),
                 title: title,
-                text: text, 
-                categorypostId: Number(categorypostId)}})
-       
-        reply.send(infop)
+                text: text,
+                categorypostId: Number(categorypostId),
+            },
+        });
+
         return reply
-        .status(204)
-        .send({ message: "InfoPost deletado com sucesso!"});
+            .status(204)
+            .send({ message: "InfoPost deletado com sucesso!" });
     } catch (error) {
         return reply
-        .status(500)
-        .send({ error: "Erro interno do servidor"});
+            .status(500)
+            .send({ error: "Erro interno do servidor" });
     }
-};
+}
