750a0d7a5daeacb0bb03c44d468cde0cf445702a
higucavalcanti <cavalcanti.higor1@gmail.com>
2023-09-12 19:42:45 -0300

    Criando HintType

---
+++ frontend/src/components/UI/HintType/index.tsx
@@ -0,0 +1,56 @@
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { ReactNode, useEffect, useState } from "react";
+import { Person, ForkKnife, Bicycle, Bag  } from "phosphor-react-native";
+
+interface EcoCateg{
+  name: String;
+}
+interface HintTypeProps {
+    ecohints: string;
+    icone: ReactNode;
+    cor: string;
+  }
+  
+
+const HintType = ({ecohints, icone, cor}: HintTypeProps) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={[styles.button , {borderColor: cor}]}>
+        {icone}
+        <Text style={styles.buttonText}>{ecohints}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default HintType;
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: "#1D3623",
+  },
+  title: {
+    color: "#C8E6C9",
+    fontSize: 35,
+    marginTop: 40,
+    fontWeight: "600",
+  },
+  button: {
+    backgroundColor: "#C8E6C9",
+    width: 150,
+    height: 150,
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: '',
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: "#000",
+    fontSize: 18,
+    marginTop: 10,
+    textAlign: "center",
+  },
+});
