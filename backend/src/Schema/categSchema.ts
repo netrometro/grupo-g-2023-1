f8cc97f19b4236eb0cef611ba346d455496c6355
higucavalcanti <cavalcanti.higor1@gmail.com>
2023-09-12 19:41:49 -0300

    deletando categSchema

--- backend/src/Schema/categSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const categSchema = z.object({
-    categorypostId:     z.string(),
-    name:               z.string(),
-});
-
-export type categSchema = z.infer<typeof categSchema>;
